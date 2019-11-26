51af5ed5b7f9bc978d079b62334940abf113c616
stamen3054 <stamen3054@gmail.com>
2019-11-26 13:55:12 +0800

    Create EnyaProduct.java

---
+++ src/main/java/hu/xia/enya/model/EnyaProduct.java
@@ -0,0 +1,31 @@
+package hu.xia.enya.model;
+
+public class EnyaProduct {
+	private String model;
+	private double price;
+	private String picturePath;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getPicturePath() {
+		return picturePath;
+	}
+
+	public void setPicturePath(String picturePath) {
+		this.picturePath = picturePath;
+	}
+}
